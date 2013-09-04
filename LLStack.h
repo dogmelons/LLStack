bfc73533e9f46b86064dc499fbda37a1b9eb828f
dogmelons <kyle.easterling@gmail.com>
2013-09-03 23:16:58 -0400

    Update LLStack.h

--- LLStack.h
+++ LLStack.h
@@ -5,33 +5,33 @@ class LLStack
 {
 public:
 
-	void clear()
+	void clear()				//clears stack
 	{
 		list.clear();
 	}
 
-	bool isEmpty()
+	bool isEmpty()				//returns if stack is empty
 	{
 		return list.isEmpty();
 	}
 
-	void peek(T& out)
+	void peek(T& out)			//gets top element
 	{
 		list.peek(out);
 	}
 	
-	void pop(T& out)
+	void pop(T& out)			//gets and pops top element
 	{
 		list.peek(out);
 		list.remove();
 	}
 
-	void push(const T& data)
+	void push(const T& data)		//pushes new element
 	{
 		list.insert(data);
 	}
 
-	void print()
+	void print()				//prints stack
 	{
 		list.print();
 	}
